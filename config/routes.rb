fbd5bee2ae920db8dfd954f4d7704a8d5242840f
AryanArya2001 <74869122+AryanArya2001@users.noreply.github.com>
2022-03-08 16:39:23 +0530

    resume app

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
   root   'home#index'
   get    'edit'    => 'home#edit'
+  get    'signup' => 'users#new'
+  post    'signup' => 'users#create'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
-  resources :profiles , only: [:update]
-  resources :educations , only: [:new]
+resources :profiles , only: [:update,:show]
+  resources :educations , only: [:new,:destroy]
+  resources :experiences , only:[ :new ,:destroy]
+  resources :projects , only:[ :new ,:destroy]
 end
